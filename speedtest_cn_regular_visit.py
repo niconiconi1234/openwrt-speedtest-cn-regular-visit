1d52d7126a72a49f1a55b7ab392184a26c177f45
HaojieZhang <zhj630985214@gmail.com>
2023-07-30 15:52:42 +0800

    fix: 修改提速api为正确的api，将访问测速网的间隔改为10分钟

--- speedtest_cn_regular_visit.py
+++ speedtest_cn_regular_visit.py
@@ -3,13 +3,14 @@
 import urllib.request
 import time
 import logging
+import json
 
 # speedtest.cn提供宽带加速服务
 # 但是宽带加速服务是和公网IP绑定的，当公网IP变化时，需要重新绑定，只需要访问speedtest.cn即可
 # 因此，这个脚本定期访问speedtest.cn，以保持宽带加速服务的有效性
 
-WEBSITE_URL = 'https://www.speedtest.cn/'
-INTERVAL_SECONDS = 60*60  # 1 hour
+WEBSITE_URL = 'https://tisu-api.speedtest.cn/api/v2/speedup/reopen?source=www'
+INTERVAL_SECONDS = 10*60  # ten minutes
 
 # 配置logger
 logger = logging.getLogger(__name__)
@@ -27,9 +28,14 @@ if __name__ == '__main__':
             logger.info('Visiting %s' % WEBSITE_URL)
             response = urllib.request.urlopen(WEBSITE_URL)
             if response.code == 200:
-                logger.info('Visit speedtest.cn success')
+                data = response.read().decode('utf-8')
+                data = json.loads(data)
+                if data['code'] == 0:
+                    logger.info('提速成功')
+                else:
+                    logger.error('提速失败, %s' % data['msg'])
             else:
-                logger.error('Visit speedtest.cn failed, response code: %d' % response.code)
+                logger.error('提速失败, http code: %d' % response.code)
         except Exception as e:
             print(e)
         time.sleep(INTERVAL_SECONDS)
